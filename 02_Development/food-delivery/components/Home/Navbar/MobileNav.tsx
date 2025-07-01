e81fa60e9c8e12d2d43cb0f38c799f92dc9a076b
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-07-01 12:12:56 +0530

    Navabr done

--- 02_Development/food-delivery/components/Home/Navbar/MobileNav.tsx
+++ 02_Development/food-delivery/components/Home/Navbar/MobileNav.tsx
@@ -1,9 +1,38 @@
+import { NavLinks } from '@/constant/constant'
 import React from 'react'
+import Link from 'next/link';
+import { CgClose } from "react-icons/cg";
 
-const MobileNav = () => {
+type Props ={
+  showNavbar : boolean;
+  closeNavbar: () => void;
+}
+
+const MobileNav = ({closeNavbar, showNavbar}:Props) => {
+  
+  const navbarOpen = showNavbar?"translate-x-0":"translate-x-[-100%]";
+
   return (
     <div>
-      {/* MobileNav */}
+      {/* Overlay */}
+      <div className={`fixed ${navbarOpen} inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-screen`}></div>
+
+      {/* Navlinks */}
+      <div className={`text-white ${navbarOpen} fixed justify-center flex flex-col h-full transform transition-al duration-500 delay-300 w-[80%] sm:w-[60%] bg-blue-950 space-y-6 z-[1050]`}>
+        {
+          NavLinks.map((link) =>{
+            return <Link key={link.id} href={link.url}>
+              <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>
+                {link.label}
+              </p>
+            </Link>
+          })
+        }
+        {/* Close icon */}
+        <CgClose 
+        onClick={closeNavbar}
+        className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6"/>
+      </div>
     </div>
   )
 }
--- 02_Development/food-delivery/components/Home/Navbar/ResponsiveNav.tsx
+++ 02_Development/food-delivery/components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,19 @@
+"use client"
 import React from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
+import {useState} from 'react';
 
 const ResponsiveNav = () => {
+  
+  const [showNavbar, setShowNavbar] = useState(false);
+  const openNavbarHandler = () => setShowNavbar(true);
+  const closeNavbarHandler = () => setShowNavbar(false);
+
   return (
     <div>
-      <Nav/>
-      <MobileNav/>
+      <Nav openNavbar={openNavbarHandler} />
+      <MobileNav showNavbar={showNavbar} closeNavbar={closeNavbarHandler} />
     </div>
   )
 }
