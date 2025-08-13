3b91585ca981acfc06f03ae3bc70ecaf924d72ed
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-08-13 14:48:23 +0530

    more work to do

--- 02_Development/02_Backend/Real State/frontend/vite-project/src/App.jsx
+++ 02_Development/02_Backend/Real State/frontend/vite-project/src/App.jsx
@@ -1,15 +1,26 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import PropertyList from "./pages/PropertyList";
 import PropertyDetails from "./pages/PropertyDetails";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PropertyForm from "./pages/PropertyForm";
+
+
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<PropertyList />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/properties/:id" element={<PropertyDetails />} />
+        <Route path="/property-form" element={<PropertyForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
